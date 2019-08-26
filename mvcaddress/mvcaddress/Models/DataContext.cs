383248dd615dd28ab1004d308dff8a44a03b656d
Mariana Arnez <mariana.anez@gmail.com>
2019-08-25 20:06:20 -0400

    Solucion Final

---
+++ mvcaddress/mvcaddress/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace mvcaddress.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<mvcaddress.Models.Book> Books { get; set; }
+    }
+}
